b1446f5aed55a87904f651208996ab4e96223bb3
Confidence Ugolo <ugoloconfidence@gmail.com>
2025-02-01 01:04:30 +0000

    Debug redirect after payment

--- src/app/redirect/[hash]/page.tsx
+++ src/app/redirect/[hash]/page.tsx
@@ -1,84 +1,84 @@
-// 'use client';
+'use client';
 
-// import { decodeRedirectHash, getGeoInfo } from '@/utils/functions';
-// import { useParams, useRouter } from 'next/navigation';
-// import { useEffect } from "react";
-// import { useFormStore } from '@/stores/formStore';
-// import { recreateSession } from '@/server-actions/api.actions';
+import { decodeRedirectHash, getGeoInfo } from '@/utils/functions';
+import { useParams, useRouter } from 'next/navigation';
+import { useEffect } from "react";
+import { useFormStore } from '@/stores/formStore';
+import { recreateSession } from '@/server-actions/api.actions';
 
-// const RedirectPage = () => {
-//     const params = useParams();
-//     // const router = useRouter();
+const RedirectPage = () => {
+    const params = useParams();
+    // const router = useRouter();
 
-//     // const sessionId = useFormStore.getState().sessionId;
-//     // const formData = useFormStore.getState().formData;
-//     // const setField = useFormStore.getState().setField;
-//     // const setCurrentFormStep = useFormStore.getState().setCurrentFormStep;
-//     // const setCurrentStepIndex = useFormStore.getState().setCurrentStepIndex;
-//     // const updateStepHighlight = useFormStore.getState().updateStepHighlight;
+    // const sessionId = useFormStore.getState().sessionId;
+    // const formData = useFormStore.getState().formData;
+    // const setField = useFormStore.getState().setField;
+    // const setCurrentFormStep = useFormStore.getState().setCurrentFormStep;
+    // const setCurrentStepIndex = useFormStore.getState().setCurrentStepIndex;
+    // const updateStepHighlight = useFormStore.getState().updateStepHighlight;
 
-//     // const setSessionId = useFormStore.getState().setSessionId;
+    // const setSessionId = useFormStore.getState().setSessionId;
 
 
-//     useEffect(() => {
-//         handle();
-//     }, []);
+    useEffect(() => {
+        // handle();
+    }, []);
 
-//     async function handle() {
-//         console.log(params , process.env);
-//         alert("Tada");
+    async function handle() {
+        console.log(params , process.env);
+        alert("Tada");
         
-//         // const data = decodeRedirectHash(
-//         //     Array.isArray(params?.hash) ? params.hash[0] : params?.hash ?? ""
-//         // );
-//         // console.log("Decoded data", data , params);
+        // const data = decodeRedirectHash(
+        //     Array.isArray(params?.hash) ? params.hash[0] : params?.hash ?? ""
+        // );
+        // console.log("Decoded data", data , params);
 
-//         // if (data) {
-//         //     setField(data.key, data.value);
+        // if (data) {
+        //     setField(data.key, data.value);
 
-//         //     if (data.key == "selected_products") {
-//         //         if (sessionId.length > 0) {
-//         //             if (formData.country) {
-//         //                 setCurrentFormStep(3);
-//         //                 setCurrentStepIndex(2);
-//         //                 updateStepHighlight("product");
-//         //                 return router.push("/" + sessionId);
-//         //             } else {
-//         //                 setCurrentStepIndex(0);
-//         //                 updateStepHighlight("info");
-//         //                 return router.push("/" + sessionId);
-//         //             }
-//         //         }
+        //     if (data.key == "selected_products") {
+        //         if (sessionId.length > 0) {
+        //             if (formData.country) {
+        //                 setCurrentFormStep(3);
+        //                 setCurrentStepIndex(2);
+        //                 updateStepHighlight("product");
+        //                 return router.push("/" + sessionId);
+        //             } else {
+        //                 setCurrentStepIndex(0);
+        //                 updateStepHighlight("info");
+        //                 return router.push("/" + sessionId);
+        //             }
+        //         }
 
-//         //     }
+        //     }
 
-//         //     if (data.key == "payment") {
-//         //         setCurrentFormStep(3);
-//         //         setCurrentStepIndex(2);
-//         //         updateStepHighlight("product");
-//         //         return router.push("/" + data.value);
-//         //     }
+        //     if (data.key == "payment") {
+        //         setCurrentFormStep(3);
+        //         setCurrentStepIndex(2);
+        //         updateStepHighlight("product");
+        //         return router.push("/" + data.value);
+        //     }
 
-//         //     if (data.key == "recreate_session") {
-//         //         setField(data.key, "");
+        //     if (data.key == "recreate_session") {
+        //         setField(data.key, "");
 
-//         //         let response = await recreateSession(data.value.id,
-//         //             data.value.token
-//         //             , await getGeoInfo());
-//         //         if (response.success) {
-//         //             setCurrentStepIndex(0);
-//         //             updateStepHighlight("info");
-//         //             setSessionId('');
-//         //             return router.push("/" + response.data.id);
-//         //         }
-//         //     }
-//         // }
-//         // return router.push("/");
-//     }
+        //         let response = await recreateSession(data.value.id,
+        //             data.value.token
+        //             , await getGeoInfo());
+        //         if (response.success) {
+        //             setCurrentStepIndex(0);
+        //             updateStepHighlight("info");
+        //             setSessionId('');
+        //             return router.push("/" + response.data.id);
+        //         }
+        //     }
+        // }
+        // return router.push("/");
+    }
 
-//     return (
-//         <>Redirecting</>
-//     );
-// };
+    return (
+        <>Redirecting</>
+    );
+};
 
-// export default RedirectPage;
+export default RedirectPage;
