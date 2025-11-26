902affa8b71b55be07665cd6063379b6b1e7e361
Confidence Ugolo <ugoloconfidence@gmail.com>
2025-11-26 01:04:04 +0000

    Add Docker support and enhance Next.js configuration
    - Updated .gitignore to include Docker-related files.
    - Modified next.config.mjs to enable standalone builds, added experimental features, and configured image remote patterns.
    - Enhanced webpack configuration to support markdown file loading.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,45 @@
+// import obfuscationConfig from './src/utils/nextConfig/obfuscationConfig.js';
+// import cssHashingConfig from './src/utils/nextConfig/cssHashingConfig.js';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', // Required for Docker standalone builds
+
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
+  },
+  experimental: {
+    swcMinify: true,
+  },
+  compiler: {
+    removeConsole: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '9802',
+        pathname: '/api/get-file/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+        pathname: '/api/get-file/**',
+      },
+    ],
+  },
+  webpack: (config, ctx) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader'
+    })
+
+    // config = obfuscationConfig(config, ctx);
+    // config = cssHashingConfig(config, ctx);
+    return config
   },
 };
 
-export default nextConfig;
+export default nextConfig
